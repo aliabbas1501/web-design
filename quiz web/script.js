f866f58cbfa1f295783a92ac81c59751f49e820c
aliabbas1501 <alii.abbaswork@gmail.com>
2025-05-31 19:10:41 +0500

    Delete quiz web directory

--- quiz web/script.js
+++
@@ -1,8 +0,0 @@
-// Get references to the button and heading
-const changeTextBtn = document.getElementById('changeTextBtn');
-const myHeading = document.getElementById('myHeading');
-
-// Set up the click event listener
-changeTextBtn.addEventListener('click', () => {
-  myHeading.textContent = 'The heading text has been changed!';
-});
